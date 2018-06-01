6cc3d3bd310967ae7ffa74eb09c32a9f050458c9
mdroberts74 <mikedroberts88@gmail.com>
2018-06-01 14:37:30 -0500

    I added the challange by creating a function that returned an object containing job history values

--- main.js
+++ main.js
@@ -1,15 +1,3 @@
-// const ChickFilA = Object.create({}, {
-//     business: {
-//         value: "Chick-Fil-A"
-//     },
-//     employmentStart: {
-//         value: "01-01-2009"
-//     },
-//     employmentEnd: {
-//         value: "01-01-2010"
-//     }
-// })
-
 const hoganLovells = Object.create({}, {
     startDate: {
         enumerable: true,
@@ -18,7 +6,7 @@ const hoganLovells = Object.create({}, {
     },
     location: {
         enumerable: true,
-        writable: false,
+        writable: true,
         value: "Century City"
     },
     salary: {
@@ -35,9 +23,20 @@ const hoganLovells = Object.create({}, {
         enumerable: false,
         writable: true,
         value: "12/30/2011"
+    },
+
+    liked: {
+        enumerable: false,
+        writable: true,
+        value: "Solving problems."
+    },
+
+    disLiked: {
+        enumerable: false,
+        writable: true,
+        value: "long extensive travel."
     }
 })
-
 
 const talentAcquisition = Object.create({}, {
     startDate: {
@@ -64,6 +63,18 @@ const talentAcquisition = Object.create({}, {
         enumerable: false,
         writable: true,
         value: "12/30/2015"
+    },
+
+    liked: {
+        enumerable: false,
+        writable: true,
+        value: "Working with the team"
+    },
+
+    disLiked: {
+        enumerable: false,
+        writable: true,
+        value: "Being a recruiter"
     }
 })
 
@@ -76,7 +87,7 @@ const onoraGroup = Object.create({}, {
     },
     location: {
         enumerable: true,
-        writable: false,
+        writable: true,
         value: "Brentwood"
     },
     salary: {
@@ -87,99 +98,105 @@ const onoraGroup = Object.create({}, {
     title: {
         enumerable: true,
         writable: false,
-        value: "Litigation Support"
+        value: "Sr. Technical Recruiter"
     },
     endDate: {
         enumerable: false,
         writable: true,
         value: "12/30/2011"
+    },
+
+    liked: {
+        enumerable: false,
+        writable: true,
+        value: "Building relationships"
+    },
+
+    disLiked: {
+        enumerable: false,
+        writable: true,
+        value: "The enviroment"
     }
+
 })
 
-
-
-const hoganLovells = Object.create({}, {
+const staffingTechnologies = Object.create({}, {
     startDate: {
         enumerable: true,
         writable: false,
-        value: "12/15/2018"
+        value: "1/15/2017"
     },
     location: {
         enumerable: true,
         writable: false,
-        value: "Century City"
+        value: "Franklin TN."
     },
     salary: {
         enumerable: true,
         writable: false,
-        value: 90000
+        value: 60000
     },
     title: {
         enumerable: true,
         writable: false,
-        value: "Litigation Support"
+        value: "Sr. Technical Recruiter"
     },
     endDate: {
         enumerable: false,
         writable: true,
-        value: "12/30/2011"
+        value: "12/5/2017"
     }
 })
 
+// var obj = {
+//     name: name,
+//     totalScore: totalScore,
+//     gamesPlayed: gamesPlayed
+// };
 
-const hoganLovells = Object.create({}, {
-    startDate: {
-        enumerable: true,
-        writable: false,
-        value: "12/15/2018"
-    },
-    location: {
-        enumerable: true,
-        writable: false,
-        value: "Century City"
-    },
-    salary: {
-        enumerable: true,
-        writable: false,
-        value: 90000
-    },
-    title: {
-        enumerable: true,
-        writable: false,
-        value: "Litigation Support"
-    },
-    endDate: {
-        enumerable: false,
-        writable: true,
-        value: "12/30/2011"
-    }
-})
+// Challange: Write a function that will build, and return, a job object. Make sure you define an argument for each one of the properties you want to be in the object.
+function jobHistory(startDateV, locationV, salaryV, titleV, endDateV, likedV, disLikedV) {
+    const newJob = Object.create({}, {
+        startDate: {
+            enumerable: true,
+            writable: false,
+            value: startDateV
+        },
+        location: {
+            enumerable: true,
+            writable: true,
+            value: locationV
+        },
+        salary: {
+            enumerable: true,
+            writable: false,
+            value: salaryV
+        },
+        title: {
+            enumerable: true,
+            writable: false,
+            value: titleV
+        },
+        endDate: {
+            enumerable: false,
+            writable: true,
+            value: endDateV
+        },
 
+        liked: {
+            enumerable: false,
+            writable: true,
+            value: likedV
+        },
 
-const hoganLovells = Object.create({}, {
-    startDate: {
-        enumerable: true,
-        writable: false,
-        value: "12/15/2018"
-    },
-    location: {
-        enumerable: true,
-        writable: false,
-        value: "Century City"
-    },
-    salary: {
-        enumerable: true,
-        writable: false,
-        value: 90000
-    },
-    title: {
-        enumerable: true,
-        writable: false,
-        value: "Litigation Support"
-    },
-    endDate: {
-        enumerable: false,
-        writable: true,
-        value: "12/30/2011"
-    }
-})
+        disLiked: {
+            enumerable: false,
+            writable: true,
+            value: disLikedV
+        }
+
+    })
+    return newJob;
+};
+
+console.log(jobHistory("5/21/2018", "Nashville TN.", "NA", "STUDENT", "10/20/2018", "Learning", "No Money"))
