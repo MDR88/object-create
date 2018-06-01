0ad2edb793218e2f80f4405d284ac4f15cc59048
mdroberts74 <mikedroberts88@gmail.com>
2018-06-01 10:27:00 -0500

    I created my structure, index file, js and cs files for my object create exercise

---
+++ main.js
@@ -0,0 +1,185 @@
+// const ChickFilA = Object.create({}, {
+//     business: {
+//         value: "Chick-Fil-A"
+//     },
+//     employmentStart: {
+//         value: "01-01-2009"
+//     },
+//     employmentEnd: {
+//         value: "01-01-2010"
+//     }
+// })
+
+const hoganLovells = Object.create({}, {
+    startDate: {
+        enumerable: true,
+        writable: false,
+        value: "12/15/2018"
+    },
+    location: {
+        enumerable: true,
+        writable: false,
+        value: "Century City"
+    },
+    salary: {
+        enumerable: true,
+        writable: false,
+        value: 90000
+    },
+    title: {
+        enumerable: true,
+        writable: false,
+        value: "Litigation Support"
+    },
+    endDate: {
+        enumerable: false,
+        writable: true,
+        value: "12/30/2011"
+    }
+})
+
+
+const talentAcquisition = Object.create({}, {
+    startDate: {
+        enumerable: true,
+        writable: false,
+        value: "1/1/2011"
+    },
+    location: {
+        enumerable: true,
+        writable: false,
+        value: "Los Angeles"
+    },
+    salary: {
+        enumerable: true,
+        writable: false,
+        value: 60000
+    },
+    title: {
+        enumerable: true,
+        writable: false,
+        value: "IT Recruiter"
+    },
+    endDate: {
+        enumerable: false,
+        writable: true,
+        value: "12/30/2015"
+    }
+})
+
+
+const onoraGroup = Object.create({}, {
+    startDate: {
+        enumerable: true,
+        writable: false,
+        value: "1/15/2015"
+    },
+    location: {
+        enumerable: true,
+        writable: false,
+        value: "Brentwood"
+    },
+    salary: {
+        enumerable: true,
+        writable: false,
+        value: 90000
+    },
+    title: {
+        enumerable: true,
+        writable: false,
+        value: "Litigation Support"
+    },
+    endDate: {
+        enumerable: false,
+        writable: true,
+        value: "12/30/2011"
+    }
+})
+
+
+
+const hoganLovells = Object.create({}, {
+    startDate: {
+        enumerable: true,
+        writable: false,
+        value: "12/15/2018"
+    },
+    location: {
+        enumerable: true,
+        writable: false,
+        value: "Century City"
+    },
+    salary: {
+        enumerable: true,
+        writable: false,
+        value: 90000
+    },
+    title: {
+        enumerable: true,
+        writable: false,
+        value: "Litigation Support"
+    },
+    endDate: {
+        enumerable: false,
+        writable: true,
+        value: "12/30/2011"
+    }
+})
+
+
+const hoganLovells = Object.create({}, {
+    startDate: {
+        enumerable: true,
+        writable: false,
+        value: "12/15/2018"
+    },
+    location: {
+        enumerable: true,
+        writable: false,
+        value: "Century City"
+    },
+    salary: {
+        enumerable: true,
+        writable: false,
+        value: 90000
+    },
+    title: {
+        enumerable: true,
+        writable: false,
+        value: "Litigation Support"
+    },
+    endDate: {
+        enumerable: false,
+        writable: true,
+        value: "12/30/2011"
+    }
+})
+
+
+const hoganLovells = Object.create({}, {
+    startDate: {
+        enumerable: true,
+        writable: false,
+        value: "12/15/2018"
+    },
+    location: {
+        enumerable: true,
+        writable: false,
+        value: "Century City"
+    },
+    salary: {
+        enumerable: true,
+        writable: false,
+        value: 90000
+    },
+    title: {
+        enumerable: true,
+        writable: false,
+        value: "Litigation Support"
+    },
+    endDate: {
+        enumerable: false,
+        writable: true,
+        value: "12/30/2011"
+    }
+})
